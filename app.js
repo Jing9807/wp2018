5314ceb31c8d02a7406e6952911791b8cb43cc0a
Jing9807 <s93181@gmail.com>
2018-10-02 21:41:57 +0800

    nop

--- app.js
+++ app.js
@@ -2,15 +2,37 @@
 $( document ).ready(function() {
     console.log( "ready!" );
     $(".check").hide()
+       $("#eggplant").click(()=>{
+      // click event
+      console.log("click")
+       $(".ingredient").toggle(
+            $(".check").hide())
+      // $(this).flip()
+    })
+       $(".icon").click(()=>{
+      // click event
+      console.log("click")
+       $(".ingredient").toggle(
+            $(".check").hide())
+      // $(this).flip()
+    })
+
     $(".ingredient#eggplant").click(()=>{
       // click event
       console.log("click")
        $(".check#check1").toggle()
+      // $(this).flip()
     })
 $(".ingredient#chili").click(()=>{
       // click event
       console.log("click")
          $(".check#check2").toggle()
-    })
+    
+})
+$(".ingredient#garlic").click(()=>{
+// click event
+console.log("click")
+   $(".check#check3").toggle()
+})
 
 });
